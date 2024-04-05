c3949cae30fa261c71719c9f52ad06ae07281215
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-05 11:39:36 +0200

    feat: added isNaN check

--- 1/script/script.js
+++ 1/script/script.js
@@ -3,6 +3,9 @@
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
 // Ogni volta che ne crei uno, stampalo a schermo.
 
-const num = parseInt(prompt("Inserisci un numero"));
+let num;
+do {
+  num = parseInt(prompt("Inserisci un numero"));
+} while (isNaN(num));
 
 console.log(createArray(num));
