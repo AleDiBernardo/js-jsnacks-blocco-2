d77cc8c9117c803ea95973f1326e217f97286be4
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-05 12:34:08 +0200

    feat: added print func

--- 2/script/functions.js
+++ 2/script/functions.js
@@ -1,12 +1,27 @@
-
-
-function isLenghtEqual (word1, word2) {
-
+/**
+ * Check if word are equal
+ * @date 4/5/2024 - 12:32:23 PM
+ *
+ * @param {*} word1
+ * @param {*} word2
+ * @returns {boolean}
+ */
+function isLenghtEqual(word1, word2) {
   let result = false;
 
-  if(word1.length === word2.length) {
-    result = true
+  if (word1.length === word2.length) {
+    result = true;
   }
 
   return result;
+}
+
+function printWord(result, word1, word2) {
+  if (result) {
+    alert(`Word1: ${word1}\nWord2: ${word2}`);
+  } else if (word1.length > word2.length) {
+    alert(`Word1: ${word1}`);
+  } else {
+    alert(`Word2: ${word2}`);
+  }
 }
--- 2/script/script.js
+++ 2/script/script.js
@@ -3,17 +3,9 @@
 // Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
 // Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
-
 const word1 = prompt("Inserisci la prima parola");
 const word2 = prompt("Inserisci la seconda parola");
 
+const result = isLenghtEqual(word1, word2);
 
-const result =isLenghtEqual(word1, word2);
-
-if(result) {
-  alert(`Word1: ${word1}\nWord2: ${word2}`);
-} else if(word1.length > word2.length) {
-  alert(`Word1: ${word1}`);
-} else {
-  alert(`Word2: ${word2}`);
-}
+printWord(result, word1, word2);
