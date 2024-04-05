5a98b9b30234961bfe8be17e8ed7e65fc5d38656
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-05 12:48:31 +0200

    feat: removed useless comments

--- 2/script/script.js
+++ 2/script/script.js
@@ -2,9 +2,12 @@
 // Dare la possibilità di inserire due parole.
 // Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
 // Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+let word1, word2;
 
-const word1 = prompt("Inserisci la prima parola");
-const word2 = prompt("Inserisci la seconda parola");
+do {
+  word1 = prompt("Inserisci la prima parola");
+  word2 = prompt("Inserisci la seconda parola");
+} while (word1 === "" || word2 === "");
 
 const result = isLenghtEqual(word1, word2);
 
