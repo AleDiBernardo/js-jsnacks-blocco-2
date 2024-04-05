05a9c737e54c722382f10848e88b73310778e7bb
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-05 11:38:00 +0200

    feat: added js doc

--- 1/script/functions.js
+++ 1/script/functions.js
@@ -1,5 +1,11 @@
 
 
+/**
+ * Create N array
+ * @date 4/5/2024 - 11:36:33 AM
+ *
+ * @param {number} numOfArray
+ */
 function createArray(numOfArray) {
     let array = [];
     for (let i = 0; i < numOfArray; i++) {
@@ -7,6 +13,12 @@ function createArray(numOfArray) {
     }
 }
 
+/**
+ * Fill the array with 10 random number
+ * @date 4/5/2024 - 11:36:43 AM
+ *
+ * @param {number} array
+ */
 function fillArray(array) {
     for (let i = 0; i < 10; i++) {
         array[i] = randNum();
@@ -15,6 +27,12 @@ function fillArray(array) {
     console.log(array);
 }
 
+/**
+ * Generate rand numbers
+ * @date 4/5/2024 - 11:36:55 AM
+ *
+ * @returns {number}
+ */
 function randNum() {
     return Math.floor(Math.random() * 100) + 1;
 }
