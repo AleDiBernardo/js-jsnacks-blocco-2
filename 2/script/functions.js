29cb35b032120a6a112bd453cde1a0ed735362a4
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-05 12:36:23 +0200

    feat: added js doc

--- 2/script/functions.js
+++ 2/script/functions.js
@@ -2,8 +2,8 @@
  * Check if word are equal
  * @date 4/5/2024 - 12:32:23 PM
  *
- * @param {*} word1
- * @param {*} word2
+ * @param {string} word1
+ * @param {string} word2
  * @returns {boolean}
  */
 function isLenghtEqual(word1, word2) {
@@ -16,6 +16,14 @@ function isLenghtEqual(word1, word2) {
   return result;
 }
 
+/**
+ * Print all word if equal else the longest
+ * @date 4/5/2024 - 12:34:39 PM
+ *
+ * @param {boolean} result
+ * @param {string} word1
+ * @param {string} word2
+ */
 function printWord(result, word1, word2) {
   if (result) {
     alert(`Word1: ${word1}\nWord2: ${word2}`);
